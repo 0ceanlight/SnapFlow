0c8fe606a56a9157cc2d4a7791ceaab2f8e1e6a4
0ceanlight <imahacubak@gmail.com>
2026-03-07 11:10:56 -0600

    Initial Commit

---
+++ SnapFlow/SnapFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapFlowApp.swift
+//  SnapFlow
+//
+//  Created by Francesca Frederick on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
